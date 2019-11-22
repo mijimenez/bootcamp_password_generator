3895754ce5a10b7626ca62f8febb23a322f97e32
Madeline Jimenez <madelineijimenez@gmail.com>
2019-11-22 00:33:36 -0500

    Add copy to clipboard function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
-// -------- Create variables
+// -------- CREATE VARIABLES
 // -------------------------
+// Target generate passcode button
+var genratePasswordBtn = document.querySelector(".gen-password-btn");
+
 // Create uppercase array
 var alphabetsListUp = [];
 var i = 65;
@@ -30,11 +33,14 @@ var specialCharsList = specialChars.split(" ");
 for(var m = 0; m < specialCharsList.length; m++) {
     
 }
-        
- // -------- Create functions
-// -------------------------       
-// create character length functions
-function askCharLength() {
+     
+
+
+
+ // -------- CREATE FUNCTIONS
+// -------------------------    
+
+function generatePassword() {
     // have them choose a character length
     var lengthAnswer = prompt("Choose password length (must be between 8 and 128 characters)");
     var passLength = parseInt(lengthAnswer);
@@ -49,91 +55,101 @@ function askCharLength() {
         alert("Please select a character length between 8 and 128.");
         askCharLength();
     }
-}
 
 
-// create character type functions
-function askCharType(passLength) {
-    // have them choose at least one character type
-    alert("Choose at least one character type from: Special Characters, Numerical Characters, Lowercase Characters and Uppercase characters");
+    // create character type functions
+    function askCharType(passLength) {
+        // have them choose at least one character type
+        alert("Choose at least one character type from: Special Characters, Numerical Characters, Lowercase Characters and Uppercase characters");
 
-    // Count how many character types they are selecting
-    var charTypeValidate = 0;
+        // Count how many character types they are selecting
+        var charTypeValidate = 0;
 
-    var charSpecialType = confirm("Want special characters in your password?");
+        var charSpecialType = confirm("Want special characters in your password?");
 
-    var charNumType = confirm("Want numerical characters in your password?");
+        var charNumType = confirm("Want numerical characters in your password?");
 
-    var charLowType = confirm("Want lowercase characters in your password?");
+        var charLowType = confirm("Want lowercase characters in your password?");
 
-    var charUpType = confirm("Want uppercase characters in your password?");
+        var charUpType = confirm("Want uppercase characters in your password?");
 
-    if (charSpecialType === true) {
-        charTypeValidate++;
-    }
-    if (charNumType === true) {
-        charTypeValidate++;
-    }
-    if (charLowType === true) {
-        charTypeValidate++;
-    }
-    if (charUpType === true) {
-        charTypeValidate++;
-    }
-
-    //log the number of character types chosen
-    console.log(charTypeValidate);
-
-    // validate at least one character type was chosen before generating a password
-    if (charTypeValidate >= 1) {
-        alert("Let's generate a password!!");
-    }
-    else {
-        alert("Please select at least one character type.");
-        askCharType();
-    }
-    
-    // If all character types are chosen...
-    var allParams = [];
-    var insertElement = function(type) {
-        for (i = 0; i < type.length; i++) {
-            allParams.push(type[i]);
+        if (charSpecialType === true) {
+            charTypeValidate++;
         }
+        if (charNumType === true) {
+            charTypeValidate++;
+        }
+        if (charLowType === true) {
+            charTypeValidate++;
+        }
+        if (charUpType === true) {
+            charTypeValidate++;
+        }
+
+        //log the number of character types chosen
+        console.log(charTypeValidate);
+
+        // validate at least one character type was chosen before generating a password
+        if (charTypeValidate >= 1) {
+            alert("Let's generate a password!!");
+        }
+        else {
+            alert("Please select at least one character type.");
+            askCharType();
+        }
+
+        // If all character types are chosen...
+        var allParams = [];
+        var insertElement = function(type) {
+            for (i = 0; i < type.length; i++) {
+                allParams.push(type[i]);
+            }
+        }
+
+        if (charSpecialType) {
+            insertElement(specialCharsList);
+        }
+        if (charNumType) {
+            insertElement(numbersList);
+        }
+        if (charLowType) {
+            insertElement(alphabetsListLow);
+        }
+        if (charUpType) {
+            insertElement(alphabetsListUp);
+        }
+        console.log(allParams);
+
+        var allParamsPw = [];
+        console.log(passLength);
+        for(var n = 0; n < passLength; n++) {
+            allParamsPw.push(allParams[Math.floor(Math.random() * allParams.length)]);
+        }
+
+        console.log("All params: " + allParamsPw);
+        // securePasscode.textContent = allParamsPw;
+        document.getElementById("passcode").value = allParamsPw;
+
     }
 
-    if (charSpecialType) {
-        insertElement(specialCharsList);
-    }
-    if (charNumType) {
-        insertElement(numbersList);
-    }
-    if (charLowType) {
-        insertElement(alphabetsListLow);
-    }
-    if (charUpType) {
-        insertElement(alphabetsListUp);
-    }
-    console.log(allParams);
-
-    var allParamsPw = [];
-    console.log(passLength);
-    for(var n = 0; n < passLength; n++) {
-        allParamsPw.push(allParams[Math.floor(Math.random() * allParams.length)]);
-    }
-
-    console.log("All params: " + allParamsPw);
 }
 
+function copyToClipboard() {
+    var copyText = document.getElementById("passcode");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    alert("Copied the text: " + copyText.value);
+  }
+
+  
+//   Questions:
+//   1. How do I remove commas from the generated password?
+//   2. Can you copy text from paragraph to Clipboard, or does this only work with input fields?
 
 
 
 
-
-// -------- Run Application
-// -------------------------
-
-// run character length function
-askCharLength();
 
 
 
